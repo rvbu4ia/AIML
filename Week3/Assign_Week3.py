761130486d9d394e51560533209f1886f36fd860
Rajendra Varma <rajendra.varma@gmail.com>
2024-08-09 17:09:08 +0530

    Modified the Week3 Assignment related program using numpy

--- Week3/Assign_Week3.py
+++ Week3/Assign_Week3.py
@@ -1,42 +1,36 @@
 import sys
+import numpy as np
 
-x1 = [60, 67, 71, 75, 78]
-x2 = [22, 24, 15, 20, 16]
-y = [140, 159, 192, 200, 212]
+x1 = np.array([60, 67, 71, 75, 78])
+x2 = np.array([22, 24, 15, 20, 16])
+y = np.array([140, 159, 192, 200, 212])
 
 w0, w1, w2 = (0, 1, 1)
 
 alpha = 0.0002
 
 def hypothesis (x1, x2, w0, w1, w2):
-    y_pred = []
-    for i in range(len(y)):
-        y_pred.append(w0 + w1 * x1[i] + w2 * x2[i])
-    return y_pred
+    return (w0 + w1*x1 + w2*x2)
 
 def mse_cost_func (y, y_pred):
-    mse = 0
-    for i in range(len(y)):
-        mse += (y[i] - y_pred[i]) ** 2
-    return (mse / (2*len(y)))
+    return np.mean((y - y_pred) ** 2)/2
 
 def gradient_descent (x1, x2, y, w0, w1, w2, alpha, iter = 0):
     w0_new, w1_new, w2_new = w0, w1, w2
     i = 0
     while True:
-        y_new = hypothesis(x1, x2, w0_new, w1_new, w2_new)
-        prev_mse = mse_cost_func(y, y_new)
-        for j in range(len(y)):
-            w0_new += alpha * (y[j] - y_new[j]) / len(y)
-            w1_new += alpha * (y[j] - y_new[j]) * x1[j] / len(y)
-            w2_new += alpha * (y[j] - y_new[j]) * x2[j] / len(y)
-        new_mse = mse_cost_func(hypothesis(x1, x2, w0_new, w1_new, w2_new), y)
+        y_pred = hypothesis(x1, x2, w0_new, w1_new, w2_new)
+        prev_mse = mse_cost_func(y, y_pred)
+        w0_new += alpha * np.mean(y - y_pred)
+        w1_new += alpha * np.mean((y - y_pred) * x1)
+        w2_new += alpha * np.mean((y - y_pred) * x2)
+        new_mse = mse_cost_func(y, hypothesis(x1, x2, w0_new, w1_new, w2_new))
         print(f"Values of w0, w1, w2 and MSE after iter {i+1} are:\n\tw0 = {w0_new}\n\tw1 = {w1_new}\n\tw2 = {w2_new}\n\tMSE = {new_mse}")
-        if round(float(prev_mse - new_mse), 4) <= 0.0001 or (i + 1 == iter):
+        if (i + 1 == iter) or (round(float(prev_mse - new_mse), 4) <= 0.0001) :
             break
         i += 1
     print("Final prediction of y: ", hypothesis(x1, x2, w0_new, w1_new, w2_new))
-    print("Final MSE: ", mse_cost_func(y, hypothesis(x1, x2, w0_new, w1_new, w2_new)))
+    print("Final MSE: ", new_mse)
 
 if __name__ == '__main__':
     iter = int(input("Enter no of preferred iterations. Enter '0' if no specific preference: "))
